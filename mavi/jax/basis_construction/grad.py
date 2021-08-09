order bug in gradient() fixed

mavi/jax/basis_construction/grad.py
@@ -36,6 +36,7 @@ def initialize(X):
 def init_candidates(X, **kwargs):
     npoints, ndims = X.shape
     dX = np.tile(np.identity(ndims), (npoints, 1))
+
     return Intermidiate(X, dX)
 
 
@@ -51,7 +52,6 @@ def construct_basis_t(cands, intermidiate, eps, gamma=1e-9):
     dCtX_ = res(dCtX, dFX, L)
 
     d, V = matrixfact_gep(CtX_, dCtX_, gamma=gamma)
-    # print(d)
 
     FtX = CtX_ @ V[:, d>eps]
     dFtX = dCtX_ @ V[:, d>eps]

mavi/numpy/evaluation/numerical_evaluation.py
@@ -11,6 +11,7 @@ def evaluate(basis, X, target='vanishing'):
         print('unknown mode: %s' % target)
         exit()
 
+
 def _evaluate_nv(B, X, device='cpu'):
     F = B.nonvanishings()
     
@@ -19,7 +20,6 @@ def _evaluate_nv(B, X, device='cpu'):
     Z0 = F[0].eval(np.ones((N, 1)))
     if len(F) == 1: return Z0
     
-    # Z1 = torch.hstack([Z0, X]) @ F[1]
     Z1 = F[1].eval(Z0, X)
     Z = np.hstack([Z0, Z1])
 
@@ -27,7 +27,6 @@ def _evaluate_nv(B, X, device='cpu'):
 
     for t in range(2, len(F)):
         C = blow(Z1, Zt)
-        # Zt = torch.hstack([Z, C]) @ F[t]
         Zt = F[t].eval(Z, C)
         Z = np.hstack([Z, Zt])
 
@@ -37,8 +36,6 @@ def _evaluate_nv(B, X, device='cpu'):
 def _evaluate_v(B, X, device='cpu'):
     F = B.nonvanishings()
     G = B.vanishings()
-    # if torch.all(torch.tensor([gt.numel()==0 for gt in G])):
-    #     return torch.zeros(len(X), 0, device=device)
 
     N = X.shape[0]
 
@@ -76,10 +73,9 @@ def _gradient_nv(B, X):
     dZ0 = np.zeros((npoints*ndims, 1))
     if len(F) == 1: 
         return dZ0
-    
+
     Z1 = F[1].eval(Z0, X)
-    # dZ1 = np.repeat(F[1][1:, :], npoints, axis=0)
-    dZ1 = np.repeat(F[1].V, npoints, axis=0)
+    dZ1 = np.tile(F[1].V, (npoints, 1))
     Z, dZ =  np.hstack((Z0, Z1)), np.hstack((dZ0, dZ1))
     Zt, dZt = deepcopy(Z1), deepcopy(dZ1)
 
@@ -102,17 +98,15 @@ def _gradient_v(B, X):
         return dZF0
     
     ZF1 = F[1].eval(ZF0, X)
-    # dZF1 = np.repeat(F[1].matrix_form()[1:, :], npoints, axis=0)
-    dZF1 = np.repeat(F[1].V, npoints, axis=0)
+    dZF1 = np.tile(F[1].V, (npoints, 1))
     ZF, dZF =  np.hstack((ZF0, ZF1)), np.hstack((dZF0, dZF1))
     ZFt, dZFt = deepcopy(ZF1), deepcopy(dZF1)
 
-    dZ = np.repeat(G[1].V, npoints, axis=0)
+    dZ = np.tile(G[1].V, (npoints, 1))
 
     for t in range(2,len(F)):
         C, dC = dblow(ZF1, ZFt, dZF1, dZFt)
         ZFt = F[t].eval(ZF, C)
-        dZFt  = F[t].eval(dZF, dC)
         dZFt  = F[t].eval(dZF, dC)
         dZt  = G[t].eval(dZF, dC)
         ZF, dZF = np.hstack((ZF, ZFt)), np.hstack((dZF, dZFt))

mavi/torch/util/plot.py
@@ -25,7 +25,7 @@ def _plot2d(vi, X, target='vanishing', n=1000, scale=1.5, x_max=1.0, y_max=1.0, 
 
     ## set plot range
     m = X.mean(dim=0)
-    x_max = y_max = X.abs().max()# np.max(np.abs(X))
+    x_max = y_max = X.abs().max().item()# np.max(np.abs(X))
     # x = np.arange(-scale*x_max, scale*x_max, resolution)
     # y = np.arange(-scale*y_max, scale*y_max, resolution)
     x = torch.linspace(-scale*x_max, scale*x_max, 50)

mavi/torch/util/util.py
@@ -30,10 +30,23 @@ def res(C, F, L):
     return C - F @ L
 
 def matrixfact(C):
-    V, d, _ = torch.linalg.svd(C.T @ C, full_matrices=True)
-    d = d**0.5
-    return d, V
+    _, d, Vt = torch.linalg.svd(C, full_matrices=True)
+    d = torch.cat((d, torch.zeros(Vt.shape[0] - len(d))))
+    return d, Vt.T
 
+def indirect_ged(A, B, gamma=1e-9):
+    '''
+    Reduce GED to two SVD
+    '''
+    Vb, db, _ = torch.linalg.svd(B)
+    db += gamma 
+    Vb_ = Vb / (db**0.5)
+
+    A_ = Vb_.T @ A @ Vb_
+    Va, d, _ = torch.linalg.svd(A_)
+    V = Vb_ @ Va
+
+    return d, V
 
 def matrixfact_gep(C, N, gamma=1e-9):
     '''
@@ -42,25 +55,17 @@ def matrixfact_gep(C, N, gamma=1e-9):
     '''
     A = C.T @ C
     B = N.T @ N
-
     r = torch.linalg.matrix_rank(B, gamma)
     gamma_ = B.diag().mean()*gamma
-    # db, Vb = torch.linalg.eigh(B+gamma_*torch.eye(B.shape[0]))
-    Vb, db, _ = torch.linalg.svd(B, full_matrices=True)
-    db += gamma_
-    Vb_ = Vb * 1/(db**0.5)
-    A_ = Vb_.T @ A @ Vb_
-    # d, Va = torch.linalg.eigh(A_)
-    Va, d, _ = torch.linalg.svd(A_, full_matrices=True)
-    V = Vb_ @ Va
+    d, V = indirect_ged(A, B, gamma=gamma_)
     d = d.abs()**0.5
-    
+
     gnorms = torch.diag(V.T@B@V)
     valid = torch.argsort(-gnorms)[:r]
 
     d, V = d[valid], V[:, valid]
     gnorms = gnorms[valid]
-
+   
     perm = torch.argsort(-d)
 
     return d[perm], V[:, perm]